d7a6ef6705bfdf621b8ed76d2a2e13d87087aeaf
AnibalDiaz <anibalmauriciod@gmail.com>
2021-03-11 12:29:10 -0600

    primerCommit

---
+++ web/index.php
@@ -0,0 +1,28 @@
+<?php
+// imprimir en pantalla
+    echo "hola mundo </br>";
+    print ("esto es una cadena impresa por print </br>");
+
+// variables
+    $n = 1;
+    $altura = 1.6;
+
+// cadena
+    $str = 'esto es una cadena $altura';
+    $str2 = 'estos es otra cadena $n';
+
+//array
+    $array = array('nombre' => "Juan",
+                    'edad' => "24",
+                    'domicilio'  => "calle manzana 115");
+    $paises = array('Mexico', 'Chile', 'Argentina');
+
+    $array['nombre'];
+    $paises[1];
+
+    print_r($array);
+    var_dump($paises);
+    var_dump($hola)
+
+
+?>
