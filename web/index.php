5fdb8c474c59cd68cd28afa515795d2f119721f5
AnibalDiaz <anibalmauriciod@gmail.com>
2021-03-11 13:58:48 -0600

    adding_db

--- web/index.php
+++ web/index.php
@@ -22,6 +22,35 @@
 
     print_r($array);
     var_dump($paises);
+    
 
+    $dbopts = parse_url(getenv('http://localhost/phpmyadmin/db_structure.php?server=1&db=dbprueba_heroku'));
+    $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["herokuAdmin"],
+                   'password' => $dbopts["12345"],
+                   'host' => $dbopts["localhost"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/dbprueba_heroku')
+                   )
+               )
+);
+
+$app->get('/db/', function() use($app) {
+    $st = $app['pdo']->prepare('SELECT name FROM test_table');
+    $st->execute();
+  
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+      $app['monolog']->addDebug('Row ' . $row['name']);
+      $names[] = $row;
+    }
+  
+    return $app['twig']->render('database.twig', array(
+      'names' => $names
+    ));
+  });
 
 ?>
